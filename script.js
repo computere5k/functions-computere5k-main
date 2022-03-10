62d2eb14c8236effb3a80a18b4ccbdcf09a718ea
computere5k <53963726+computere5k@users.noreply.github.com>
2022-03-09 20:25:31 -0700

    Created String Variables

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 function addTwoNumbers(a, b) {
   var result = a + b;
+  document.getElementById("named-label").innerHTML = namedLabel;
   document.getElementById("named-function").innerHTML =
     a + " + " + b + " = " + result;
-  console.log("Named Function Result: " + result);
+  console.log(namedLabel + ": " + a + " + " + b + " = " + result);
 }
 
 var firstNumber = 5;
 var secondNumber = 4;
 var thirdNumber = 32;
 var fourthNumber = 45;
+var namedLabel = "Named Function";
+var anonLabel = "Anonymous Function";
 
 addTwoNumbers(firstNumber, secondNumber);
 
 var addSomethingElse = function () {
   var result = thirdNumber + fourthNumber;
+  document.getElementById("anon-label").innerHTML = anonLabel;
   document.getElementById("anon-function").innerHTML =
     thirdNumber + " + " + fourthNumber + " = " + result;
-  console.log("Anonymous Function Result: " + result);
+  console.log(
+    anonLabel + ": " + thirdNumber + " + " + fourthNumber + " = " + result
+  );
 };
 
 addSomethingElse();
